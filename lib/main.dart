947674b12bd036660090652aa101854bc6642626
chunsu.kim <ppopuy88@gmail.com>
2022-03-19 18:35:57 +0900

    제목 수정

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '제목 변경했다',
       theme: ThemeData(
         // This is the theme of your application.
         //
